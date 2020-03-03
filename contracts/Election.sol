c92fca926aa3a39bc32dcd55b7a29b57852d2583
Jason Tiller <49928484+Tillerdawg@users.noreply.github.com>
2020-03-03 16:24:35 -0500

    Create Candidate data structure; mapping, and addCandidate function.

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,25 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Store Candidates
+    mapping(uint => Candidate) public candidates;
+    // Fetch Candidate
+    // Store Candidates Count
+    uint public candidatesCount;
+
+
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
     }
 
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 }
