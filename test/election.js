0f352a3b9b995207c51b5072c0ac5d3f0fcfa6b0
Jason Tiller <49928484+Tillerdawg@users.noreply.github.com>
2020-03-04 09:32:01 -0500

    debug election.js

--- test/election.js
+++ test/election.js
@@ -2,7 +2,7 @@ var Election = artifacts.require("./Election.sol");
 
 contract("Election", function(accounts){
     it("initializes with two candidates", function(){
-        return Election.deployed().the(function(instance){
+        return Election.deployed().then(function(instance){
             return instance.candidatesCount();
         }).then(function(count) {
             assert.equal(count, 2);
